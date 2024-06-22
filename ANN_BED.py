fc44dcd0ec33e16461eecdb82dccdc14876a0658
rrr259 <renata.borisova99@gmail.com>
2024-06-22 15:46:56 +0000

    new ANN_BED

--- ANN_BED.py
+++ ANN_BED.py
@@ -3,13 +3,14 @@ import os
 import subprocess
 import time 
 
-bed_file = 'ann_promoters.bed'
+bed_file = 'ann_to_m39.bed'
 print('Modifying BED file for further analyis')
 print('--------------------------------------')
 
 #need tp generate genome.sizes from fasta file 
 fasta_file = 'Mus_musculus.GRCm39.dna_sm.primary_assembly.fa'
 fasta_to_fai = subprocess.run('samtools faidx ' + fasta_file, shell=True)
+time.sleep(0.5)
 if fasta_to_fai.returncode !=0:
     print('Error occured')
 else: 
@@ -20,8 +21,12 @@ else:
 new_fai = fasta_file + '.fai'
 gen_size = 'genome.size_now'
 fai_to_gen = subprocess.run("awk '{{print $1\"\t\"$2}}' " + new_fai + " > " + gen_size, shell=True)
-print('Created genome.size file')
-print('-------------------------')
+time.sleep(0.5)
+if fai_to_gen.returncode !=0:
+    print('Error occured')
+else: 
+    print('Created genome.size file')
+    print('-------------------------')
 
 gen_size_new = 'genome.size'
 
@@ -32,11 +37,17 @@ with open(gen_size, 'r') as infile, open(gen_size_new, 'w') as uotfile:
             new_line = 'chr' + new_line
             uotfile.write(new_line +'\n')
 
+#remove genome.size without chr 
 os.remove(gen_size)
-        
 
 #using flank to modify bed 
+time.sleep(0.5)
 bed_to_new = subprocess.run('flankBed -i ' + bed_file + ' -g ' + gen_size_new + ' -b 500 > new_bed.bed', shell=True)
-print('BED file now modified')
+time.sleep(0.5)
+if bed_to_new.returncode !=0:
+    print('Error occured')
+else:
+    print('BED file now modified')
+    print('---------------------')
 
 
